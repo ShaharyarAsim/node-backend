b194c3a004384e491fa387be77bdc2785822d700
Shaharyar <91311749+ShaharyarAsim@users.noreply.github.com>
2023-10-20 13:14:13 +1100

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
